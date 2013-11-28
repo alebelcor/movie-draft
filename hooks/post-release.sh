8e168283bb3ae9f71dd0150c5ee853e224c8c8f2
alebelcor <856838+alebelcor@users.noreply.github.com>
2013-11-28 14:02:45 -0700

    Update post-release.sh

--- hooks/post-release.sh
+++ hooks/post-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # INFO: This not an official Git hook
-# see `git-release` of git-extras (https://github.com/visionmedia/git-extras)
+# @see `git-release` of git-extras (https://github.com/visionmedia/git-extras)
 #
 # This is called after `git release`
 
